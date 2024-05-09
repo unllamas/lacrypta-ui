e59dac93706b0845cb69d9730c64b68391f7a732
Agustin Kassis <webmaster@masize.com>
2024-05-09 14:27:48 -0300

    updated html header

--- apps/docs/theme.config.jsx
+++ apps/docs/theme.config.jsx
@@ -1,5 +1,19 @@
 export default {
   logo: <span>lacrypta-ui</span>,
+  head: (
+    <head>
+      <meta property="og:title" content="La CryptaUI Documentation" />
+      <meta property="og:description" content="The official ui documentation for LaCrypta UI." />
+      <meta property="og:image" content="/favicon.ico" />
+      <link rel="icon" href="/favicon.ico" sizes="any" />
+      <link rel="shortcut icon" href="/favicon.ico" />
+    </head>
+  ),
+  useNextSeoProps() {
+    return {
+      titleTemplate: '%s – LaCrypta UI Docs',
+    };
+  },
   project: {
     link: 'https://github.com/unllamas/lacrypta-ui',
   },
