2f476900d0807e55ae429c5696b2ff06a581cec7
Jona <llamasjonat@gmail.com>
2024-05-08 23:59:05 -0300

    fix(app/web): integrated fonts

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,5 +1,7 @@
 import { AppProvider } from '@/context/AppProvider';
 
+import '@/fonts/main.css';
+
 export default function App({ children }: { children: React.ReactNode }): JSX.Element {
   return (
     <html lang='en'>
