0f9a9d907e911015a64b2ded504bf3065e61abdc
Jona <llamasjonat@gmail.com>
2024-05-09 01:54:29 -0300

    feat(package/ui): added new icons

--- packages/ui/src/components/Icons/index.tsx
+++ packages/ui/src/components/Icons/index.tsx
@@ -1,4 +1,9 @@
 export { Check } from './Check';
 export { Alert } from './Alert';
+export { Gear } from './Gear';
+export { Hidden } from './Hidden';
+export { Receive } from './Receive';
+export { Send } from './Send';
+export { Visible } from './Visible';
 // Customs
 export { Loader } from './Loader';
