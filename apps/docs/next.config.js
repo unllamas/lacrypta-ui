0b6244d307815a09924c5ebe3e8e326392396e4f
Jona <llamasjonat@gmail.com>
2024-05-11 21:29:23 -0300

    chore: install and configurate hooks lawallet on app/docs

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -7,4 +7,10 @@ const withNextra = require('nextra')({
 
 module.exports = withNextra({
   transpilePackages: ['@unllamas/lacrypta-ui'],
+  env: {
+    PRIVATE_KEY:
+      '5caa3cd87cf1ad069bcf90065f8e3c60e18a4fca7b6070a44ec7223877504c84',
+    PUBLIC_KEY:
+      'cee287bb0990a8ecbd1dee7ee7f938200908a5c8aa804b3bdeaed88effb55547',
+  },
 });
