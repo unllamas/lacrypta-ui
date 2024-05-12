202c439ee51270fd1b31c3d1b15c3d0fa9ba9f1b
Jona <llamasjonat@gmail.com>
2024-05-11 21:30:41 -0300

    fix(app/doc): updated DashboardPreview

--- apps/docs/components/Landing/DashboardPreview.tsx
+++ apps/docs/components/Landing/DashboardPreview.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import {
   Button,
   Container,
@@ -11,15 +12,61 @@ import {
   Avatar,
   Send,
   Receive,
+  useDisclosure,
+  Drawer,
+  Input,
+  QRCode,
+  Loader,
+  Satoshi,
+  Check,
 } from '@unllamas/lacrypta-ui';
+
+// LaWallet
+import { NostrProvider, useZap } from '@lawallet/react';
+import { config } from '@/config/lawallet';
 
 import { appTheme } from '@/config/theme';
 
+type SheetTypes = 'amount' | 'qr' | 'finished';
+
 export const DashboardPreview = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const { invoice, createZapInvoice } = useZap({
+    receiverPubkey: process.env.PUBLIC_KEY || '',
+    config,
+  });
+
+  const [amount, setAmount] = useState(0);
+  // const [message, setMessage] = useState('');
+  const [sheetStep, setSheetStep] = useState<SheetTypes>('amount');
+
+  const handleCreateZap = async () => {
+    if (invoice.loading) return;
+    createZapInvoice(amount).then((bolt11: string | undefined) => {
+      if (!bolt11) {
+        alert('ERROR_ON_CREATE_INVOICE');
+        return;
+      }
+      setSheetStep('qr');
+    });
+  };
+
+  const handleCloseDrawer = () => {
+    setAmount(0);
+    // setMessage('');
+    setSheetStep('amount');
+    onClose();
+  };
+
+  useEffect(() => {
+    if (invoice.payed) setSheetStep('finished');
+  }, [invoice.payed]);
+
   return (
-    <>
+    <NostrProvider config={config}>
       <Divider y={16} />
-      <Container>
+      <Container size="md">
         <Flex justify="space-between">
           <Flex gap="sm" align="center">
             <Flex flex={0}>
@@ -92,7 +139,7 @@ export const DashboardPreview = () => {
       <Divider y={24} />
       <Container size="sm">
         <Flex flex={1} justify="center" gap="sm">
-          <Button isBlock>
+          <Button isBlock onClick={onOpen}>
             <Icon size="md" icon={<Receive />} />
             Depositar
           </Button>
@@ -236,6 +283,126 @@ export const DashboardPreview = () => {
         <Divider y={12} />
         {/* fin tx */}
       </Container>
-    </>
+      <Drawer
+        isOpen={isOpen}
+        onClose={handleCloseDrawer}
+        title="Crear factura"
+        closeText="Cerrar"
+      >
+        {sheetStep === 'amount' && (
+          <>
+            <Text isBold>Seleccioná el monto que deseas depositar</Text>
+            <Divider y="sm" />
+            <Flex gap="sm" justify="center">
+              <Button
+                variant={amount === 1 ? 'bezeled' : 'bezeledGray'}
+                onClick={() => setAmount(1)}
+                isBlock
+              >
+                1 sat
+              </Button>
+              <Button
+                variant={amount === 420 ? 'bezeled' : 'bezeledGray'}
+                onClick={() => setAmount(420)}
+                isBlock
+              >
+                420 sat
+              </Button>
+              <Button
+                variant={amount === 1000 ? 'bezeled' : 'bezeledGray'}
+                onClick={() => setAmount(1000)}
+                isBlock
+              >
+                1K sat
+              </Button>
+            </Flex>
+
+            <Divider y="sm" />
+
+            <Flex gap="sm" justify="center">
+              <Button
+                variant={amount === 10000 ? 'bezeled' : 'bezeledGray'}
+                onClick={() => setAmount(10000)}
+                isBlock
+              >
+                10K sat
+              </Button>
+              <Button
+                variant={amount === 100000 ? 'bezeled' : 'bezeledGray'}
+                onClick={() => setAmount(100000)}
+                isBlock
+              >
+                100K sat
+              </Button>
+            </Flex>
+
+            <Divider y="md" />
+
+            <Flex gap="md" align="center">
+              <Text size="small">Personalizado:</Text>
+              <Input
+                placeholder="0 sats"
+                value={amount.toString()}
+                onChange={(e) => setAmount(parseInt(e.target.value))}
+                type="number"
+              />
+            </Flex>
+
+            <Divider y="md" />
+
+            {/* <Text isBold>Te gustaría dejar un mensaje?</Text>
+            <Divider y="sm" />
+            <Input
+              placeholder={`Mensaje (opcional)`}
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+            />
+
+            <Divider y="md" /> */}
+
+            <Button isBlock disabled={amount <= 0} onClick={handleCreateZap}>
+              Crear factura
+            </Button>
+          </>
+        )}
+        {sheetStep === 'qr' && (
+          <Flex direction="column" align="center">
+            <QRCode size={250} value={`${invoice.bolt11.toUpperCase()}`} />
+            <Divider y="md" />
+            <Icon icon={<Loader />} />
+            <Divider y="md" />
+            <Flex direction="column" align="center" gap="sm">
+              <Text color={appTheme.colors.gray[500]}>Esperando pago</Text>
+              <Flex align="center" justify="center">
+                <Icon icon={<Satoshi color={appTheme.colors.gray[500]} />} />
+                <Heading as="h2">{amount}</Heading>
+              </Flex>
+            </Flex>
+          </Flex>
+        )}
+        {sheetStep === 'finished' && (
+          <>
+            <Flex direction="column" justify="center" align="center" gap="sm">
+              <Icon icon={<Check color={appTheme.colors.primary[400]} />} />
+              <Text isBold align="center">
+                Pago recibido!
+              </Text>
+              <Text color={appTheme.colors.gray[500]} align="center">
+                Felicitaciones por haber encontrado nuestro easter egg! Hemos
+                tomado tu pago como agradecimiento y colaboracion :)
+              </Text>
+              <Flex align="center" justify="center">
+                <Icon icon={<Satoshi color={appTheme.colors.gray[500]} />} />
+                <Heading as="h2">{amount}</Heading>
+              </Flex>
+            </Flex>
+            <Divider y="md" />
+            <Button variant="bezeledGray" onClick={handleCloseDrawer}>
+              Cerrar
+            </Button>
+          </>
+        )}
+      </Drawer>
+    </NostrProvider>
   );
 };
